dbd606f92cf6b1dfb732f3250d39a15423ce955b
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-07 20:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let frequencyMap = [];
+
+	for(let i = 0; i < str.length; i++){
+		let char = str[i];
+		if (char in frequencyMap) {
+			frequencyMap[char] += 1;
+		} else {
+			frequencyMap[char] = 1;
+		}
+	}
+	
+	for(let i = 0; i < str.length; i++){
+        if(frequencyMap[str[i]] === 1){
+            return str[i];
+        }
+    }
+
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
